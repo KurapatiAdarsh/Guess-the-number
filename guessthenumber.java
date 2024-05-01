2646bc2ec93ee9e0b98970f7a6ce916acd1a86c2
KurapatiAdarsh <161073800+KurapatiAdarsh@users.noreply.github.com>
2024-05-01 22:20:59 +0530

    Update guessthenumber.java

--- guessthenumber.java
+++ guessthenumber.java
@@ -5,11 +5,11 @@ class Test {
 public static void main (String args[]){
 Scanner sc = new Scanner (System.in);
 Random o1 = new Random();
-int random =o1.nextInt(20);
+int random =o1.nextInt(100);
 int choices=3;
 
 
-System.out.println("OP is generated a number between 1 to 20,match that number you have 3 choices");
+System.out.println("OP is generated a number between 1 to 100,match that number you have 3 choices");
 
 
 for (int i=1; i<=choices; i++){
