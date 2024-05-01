cddf260cbfd9c3a9a7eea2c727b59a6c07bfda57
KurapatiAdarsh <161073800+KurapatiAdarsh@users.noreply.github.com>
2024-05-01 22:34:49 +0530

    Update guessthenumber.java

--- guessthenumber.java
+++ guessthenumber.java
@@ -7,7 +7,8 @@ Scanner sc = new Scanner (System.in);
 Random o1 = new Random();
 int random =o1.nextInt(100);
 int choices=3;
-
+int cscore=0;
+int uscore=0;
 
 System.out.println("OP is generated a number between 1 to 100,match that number you have 3 choices");
 
@@ -17,6 +18,8 @@ int userinput=sc.nextInt();
 
 if (userinput>random){
 System.out.println("you are number is too High");
+
+
 }
 
 
@@ -26,12 +29,16 @@ System.out.println("your are number is too Low");
 
 else {
 System.out.println("both are equal congrationals");
+uscore++;
+System.out.println("user score is "+uscore);
 break;
 }
 
 
 if (i==choices){
 System.out.println("sorry try again");
+cscore++;
+System.out.println("computer score is "+cscore);
 }
 }//i
 
